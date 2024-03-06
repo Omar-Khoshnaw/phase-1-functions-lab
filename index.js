600ef284a914281f70d7cafbf1b5f597112cfca2
Omar Sardar <omar.khoshnaw@gmail.com>
2024-03-07 00:07:55 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+  const ScuberHeadquartersPlace = 42;
+  return pickUpLocation > ScuberHeadquartersPlace
+    ? pickUpLocation - ScuberHeadquartersPlace
+    : ScuberHeadquartersPlace - pickUpLocation;
+}
+
+function distanceFromHqInFeet(pickUpLocation) {
+  const numOfBlocks = distanceFromHqInBlocks(pickUpLocation);
+  return numOfBlocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return start > destination
+    ? (start - destination) * 264
+    : (destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  let distanceInFeet = distanceTravelledInFeet(start, destination);
+  if (distanceInFeet < 400) return 0;
+  else if (distanceInFeet > 400 && distanceInFeet <= 2000)
+    return (distanceInFeet - 400) * 0.02;
+  else if (distanceInFeet > 2000 && distanceInFeet <= 2500) return 25;
+  else if (distanceInFeet > 2500) return "cannot travel that far";
+}
